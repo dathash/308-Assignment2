a035d4a4573e2c74d2b87891944a07deca227e78
dathash <alexanderhartford@gmail.com>
2021-01-12 09:44:31 -0800

    Delete

--- sampleBackend.py
+++ sampleBackend.py
@@ -50,14 +50,23 @@ def get_users():
         resp.status_code = 200
         return resp
 
-@app.route('/users/<id>')
+@app.route('/users/<id>', methods=["GET", "DELETE"])
 def get_user(id):
-    if id:
-        for user in users['users_list']:
-            if user['id'] == id:
-                return user
-        return ({})
-    return users
+    if request.method == "GET":
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    return user
+            return ({})
+        return users
+    elif request.method == "DELETE":
+        if id:
+            for user in users['users_list']:
+                if user['id'] == id:
+                    users['users_list'].remove(user)
+        resp = jsonify(success=True)
+        resp.status_code = 200
+        return resp
 
 @app.route('/')
 def hello_world():
